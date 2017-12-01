80755581f01ced9abd7a8dae3fb22b42b489cfba
iodbh <iodbh@iodbh.net>
2017-12-01 08:53:38 +0100

    Solved part 2 of day 1

--- 2017_day_1_solution.py
+++ 2017_day_1_solution.py
@@ -1,6 +1,6 @@
 from utils import get_input
 
-def solution1(input):
+def solution_1_1(input):
     input = [int(x) for x in input.strip()]
     last = len(input)-1
     output = 0
@@ -14,5 +14,16 @@ def solution1(input):
         if next_idx == 0:
             return output
 
+def solution_1_2(input):
+    input = [int(x) for x in input.strip()]
+    output = 0
+    nsteps = int(len(input)/2)
+    for idx, val in enumerate(input):
+        next_idx = (idx+nsteps)%len(input)
+        if val == input[next_idx]:
+            output += val
+    return output
+
 if __name__ == '__main__':
-    print(solution1(get_input(2017,1)))
+    print(solution_1_1(get_input(2017,1)))
+    print(solution_1_2(get_input(2017,1)))

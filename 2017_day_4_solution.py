51a24e18c153da06a333111a17c962541eeb0fcf
iodbh <iodbh@iodbh.net>
2017-12-04 11:33:12 +0100

    Added solution for day 4 part 2 and removed print() calls

--- 2017_day_4_solution.py
+++ 2017_day_4_solution.py
@@ -7,17 +7,27 @@ def parse_input(input):
 
 def solution_4_1(input):
     valid_count = len(input)
-    print(valid_count)
     for line in input:
         while len(line):
             word = line.pop()
             if word in line:
                 valid_count -= 1
-                print('invalid')
                 break
-    print(valid_count)
+    return valid_count
+
+
+def solution_4_2(input):
+    valid_count = len(input)
+    for line in input:
+        split_line = [sorted(list(w)) for w in line]
+        while len(split_line):
+            word = split_line.pop()
+            if word in split_line:
+                valid_count -= 1
+                break
     return valid_count
 
 
 if __name__ == '__main__':
     print(solution_4_1(parse_input(get_input(2017, 4))))
+    print(solution_4_2(parse_input(get_input(2017, 4))))

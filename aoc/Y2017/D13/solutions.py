1164695a39244304a9f89a8a866d41af9ce0cbec
iodbh <iodbh@iodbh.net>
2017-12-14 11:14:06 +0100

    Added a proper solution from day13 (from reddit)

--- aoc/Y2017/D13/solutions.py
+++ aoc/Y2017/D13/solutions.py
@@ -1,90 +1,27 @@
+"""
+This solution is from https://www.reddit.com/r/adventofcode/comments/7jgyrt/2017_day_13_solutions/
+"""
+
 from ...classes import Solution
-
-
-class Scanner:
-    def __init__(self, range):
-        self.range = range
-        self.start_position = 0
-        self.end_position = 0
-        self.direction = 1
-
-    @property
-    def positions(self):
-        return self.start_position, self.end_position
-
-    def step(self):
-        if self.range is not None:
-            self.start_position = self.end_position
-            if (self.direction == 1 and self.end_position == self.range - 1) or (self.direction == -1 and self.end_position == 0):
-                self.direction *= -1
-            self.end_position = self.end_position + self.direction
-        else:
-            self.start_position = self.end_position = None
-
-
-class Firewall:
-    def __init__(self, scanners):
-        self.scanners = scanners
-        self.state = [(0,0) for scanner in self.scanners]
-        self.width = len(self.scanners)
-
-    def step(self):
-        for scanner in self.scanners:
-            scanner.step()
-        self.state = [scanner.positions for scanner in self.scanners]
-
-    def get_severity(self, position):
-        scanner = self.scanners[position]
-        if scanner.start_position == 0:
-            return position*scanner.range
-        return 0
-
-    def detected(self, position):
-        if position is None:
-            return False
-        scanner = self.scanners[position]
-        if scanner.start_position == 0:
-            return True
-        return False
-
-    def reset(self):
-        for scanner in self.scanners:
-            scanner.__init__(scanner.range)
+import itertools
 
 
 def parse_input(data):
-    ranges = {}
-    for line in data:
-        depth, scanner_range = (int(i.strip()) for i in line.split(':'))
-        ranges[depth] = scanner_range
-    return [Scanner(ranges.get(i, None)) for i in range(max(ranges.keys()) + 1 )]
+    return {int(pos): int(height) for pos, height in (line.split(': ') for line in data)}
+
+
+def scanner(height, time):
+    offset = time % ((height - 1) * 2)
+
+    return 2 * (height - 1) - offset if offset > height - 1 else offset
 
 
 def phase1(data):
-    firewall = Firewall(data)
-    total_severity = 0
-    for i in range(firewall.width):
-        firewall.step()
-        total_severity += firewall.get_severity(i)
-    return total_severity
+    return sum(pos * data[pos] for pos in data if scanner(data[pos], pos) == 0)
 
 
 def phase2(data):
-    firewall = Firewall(data)
-    delay = 0
-    while True:
-        positions = [None for picosecond in range(delay)]
-        positions.extend(range(firewall.width))
+    return next(wait for wait in itertools.count() if not any(scanner(data[pos], wait + pos) == 0 for pos in data))
 
-        detected = False
-        firewall.reset()
-        for position in positions:
-            firewall.step()
-            if firewall.detected(position):
-                detected = True
-                break
-        if not detected:
-            return delay
-        delay += 1
 
 solution = Solution(2017, 13, phase1=phase1, phase2=phase2, input_parser=parse_input)

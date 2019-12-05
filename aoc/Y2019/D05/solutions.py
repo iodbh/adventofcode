03f3517c6344f54d89331e4f727d76c70593d85f
iodbh <iodbh@iodbh.net>
2019-12-05 17:58:15 +0100

    2019 Day 05 Part 2

--- aoc/Y2019/D05/solutions.py
+++ aoc/Y2019/D05/solutions.py
@@ -3,13 +3,15 @@ from ..classes import IntCode
 
 
 def phase1(data):
-    computer = IntCode(data, debug=True)
-    state, screen_output = computer.run_program()
+    computer = IntCode(data, silent=True)
+    state, screen_output = computer.run_program(inputs=[1])
     return screen_output[-1]
 
 
 def phase2(data):
-    return None
+    computer = IntCode(data, silent=True)
+    state, screen_output = computer.run_program(inputs=[5])
+    return screen_output[0]
 
 
 solution = Solution(2019, 5, phase1=phase1, phase2=phase2, input_parser=IntCode.read_code)
--- aoc/Y2019/classes.py
+++ aoc/Y2019/classes.py
@@ -2,18 +2,28 @@ class IntCode:
     """
     An IntCode Computer
     """
+    # Note: any parameter called "dst" will not be dereferenced when parsing
+    # parameters ("Parameters that an instruction writes to will never be in
+    # immediate mode.")
     OPCODES = {
         1: {"name": "ADD", "params": ("val1", "val2", "dst")},
         2: {"name": "MUL", "params": ("val1", "val2", "dst")},
         3: {"name": "INP", "params": ("dst",)},
         4: {"name": "OUT", "params": ("src",)},
+        5: {"name": "JIT", "params": ("cond", "to")},
+        6: {"name": "JIF", "params": ("cond", "to")},
+        7: {"name": "ILT", "params": ("val1", "val2", "dst")},
+        8: {"name": "IEQ", "params": ("val1", "val2", "dst")},
         99: {"name": "EXT", "params": tuple()},
     }
 
     PAR_POSITION = 0
     PAR_IMMEDIATE = 1
 
-    def __init__(self, memory=None, debug=False,output="print"):
+    def __init__(self, memory=None, debug=False, silent=False):
+        """
+        if silent is set, OUT instructions won't actually print anything
+        """
         if memory is not None:
             self.memory = memory[:]
         else:
@@ -22,16 +32,25 @@ class IntCode:
         self.initial_memory = self.memory[:]
 
         self.ip = 0  # Instruction Pointer
-        self.screen_output = []
+
+
+        # automation stuff
+        self.inputs = []
+        self.screen_output = []  # stores output messages
+        self.silent = silent
 
         if debug:
             self.log = print
         else:
-            self.log = lambda *x: None
+            self.log = lambda *x, **y: None
 
-    def run_program(self, memory=None, ip=0):
+    def run_program(self, memory=None, ip=0, inputs=None):
         self.log("[PRG] RUNNING PROGRAM")
         self.log("[MEM] MEMORY STATE:")
+
+        if inputs is not None:
+            self.inputs = inputs[:]
+
         self.log(self.memory)
         if memory is not None:
             self.memory = memory
@@ -55,7 +74,7 @@ class IntCode:
             raise ValueError(f"Invalid opcode: {opcode}")
         self.log(f"[RUN] | {op['name']} | {' '.join(p+':'+str(v) for p, v in zip(op['params'], parameters))}")
         getattr(self, op_name)(*parameters)
-        if opcode != 99:
+        if opcode not in (5, 6, 99):  # instructions that set the next instruction
             self.ip += len(op["params"]) + 1
 
     def parse_instruction(self, address):
@@ -107,28 +126,84 @@ class IntCode:
         return value
 
     def op_ADD(self, val1, val2, dst):
+        """
+        ADDition
+        """
         self.log(f"     [ADD] mem[{dst}]={val1+val2}({val1}+{val2})")
         self.memset(dst, val1 + val2)
 
     def op_MUL(self, val1, val2, dst):
+        """
+        MULtiplication
+        """
         self.log(f"     [MUL] mem[{dst}]={val1 * val2}({val1}*{val2})")
         self.memset(dst, val1 * val2)
 
     def op_INP(self, dst):
-        try:
-            val = int(input(" > "))
-        except ValueError:
-            print("Invalid input.")
-            self.op_INP(dst)
-            return
+        """
+        INPut
+        """
+        valid = False
+        if len(self.inputs):
+            val = self.inputs.pop()
+        else:
+            while not valid:
+                try:
+                    val = int(input(" > "))
+                    valid = True
+                except ValueError:
+                    print("Invalid input.")
         self.memset(dst, val)
 
-    def op_EXT(self):
-        self.ip = None
-
     def op_OUT(self, src):
+        """
+        OUTput
+        """
         self.screen_output.append(src)
-        print(src)
+        if not self.silent:
+            print(src)
+
+    def op_JIT(self, cond, to):
+        """
+        Jump If True
+        """
+        if cond != 0:
+            self.ip = to
+        else:
+            self.ip += 3
+
+    def op_JIF(self, cond, to):
+        """
+        Jump If False
+        """
+        if cond == 0:
+            self.ip = to
+        else:
+            self.ip += 3
+
+    def op_ILT(self, val1, val2, dst):
+        """
+        Is Less Than
+        """
+        if val1 < val2:
+            self.memset(dst, 1)
+        else:
+            self.memset(dst, 0)
+
+    def op_IEQ(self, val1, val2, dst):
+        """
+        Is EQual
+        """
+        if val1 == val2:
+            self.memset(dst, 1)
+        else:
+            self.memset(dst, 0)
+
+    def op_EXT(self):
+        """
+        EXiT
+        """
+        self.ip = None
 
     def reset(self):
         self.memory = self.initial_memory[:]

20795660913ffb3a493d21736ac5d4e49672072b
iodbh <iodbh@iodbh.net>
2019-12-14 14:07:41 +0100

    removed output from day 9

--- aoc/Y2019/D09/solutions.py
+++ aoc/Y2019/D09/solutions.py
@@ -3,15 +3,14 @@ from ..classes import IntCode
 
 
 def phase1(data):
-    computer = IntCode(debug=False)
+    computer = IntCode(debug=False, silent=True)
     mem, out = computer.run_program(data, inputs=[1])
     return out[0]
 
 
 def phase2(data):
-    computer = IntCode(debug=False)
+    computer = IntCode(debug=False, silent=True)
     mem, out = computer.run_program(data, inputs=[2])
-    print(out)
     return out[0]
 
 
